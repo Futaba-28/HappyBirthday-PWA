9f27ee93c09479843830e4695abc5d97130047a4
Futaba-28 <flute.amaki@gmail.com>
2025-02-08 17:51:12 +0900

    disable app.js

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,40 @@
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open('vsm-cache').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/style.css',
-        '/app.js',
-        '/celebrate.js',
-        '/celebrate.html',
-        '/make-a-wish.html',
-        '/make-a-wish.js',
-        '/music.js',
-        'music.html',
-        '/media/images/logos/cake_192.png',
-        '/media/images/logos/cake_512.png'
-      ]);
-    })
-  );
+// self.addEventListener('install', (event) => {
+//   event.waitUntil(
+//     caches.open('vsm-cache').then((cache) => {
+//       return cache.addAll([
+//         '/',
+//         '/index.html',
+//         '/style.css',
+//         '/app.js',
+//         '/celebrate.js',
+//         '/celebrate.html',
+//         '/make-a-wish.html',
+//         '/make-a-wish.js',
+//         '/music.js',
+//         'music.html',
+//         '/media/images/logos/cake_192.png',
+//         '/media/images/logos/cake_512.png'
+//       ]);
+//     })
+//   );
+// });
+
+// self.addEventListener('fetch', (event) => {
+//   event.respondWith(
+//     caches.match(event.request).then((cachedResponse) => {
+//       return cachedResponse || fetch(event.request);
+//     })
+//   );
+// });
+
+self.addEventListener('fetch', function (e) {
+  console.log('service worker fetch')
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      return cachedResponse || fetch(event.request);
-    })
-  );
+self.addEventListener('install', function (e) {
+  console.log('service worker install')
+});
+
+self.addEventListener('activate', function (e) {
+  console.log('service worker activate')
 });
